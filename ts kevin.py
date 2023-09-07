9b9ea7a8a7e779fbeca689a91abc6bad140a91fd
kevingm26 <144158053+kevingm26@users.noreply.github.com>
2023-09-07 09:35:25 -0600

    Add files via upload

---
+++ ts kevin.py
@@ -0,0 +1,17 @@
+#Laboratorio introduccion a la programacion
+#Kevin Garcia 1228423
+#7/9/2023
+#Objetivo: que se cumpla lo que nos pide el sistema, completar los datos del usuario
+
+#Entrada
+sNombre = input("Ingresar nombre")
+sEdad = input("Ingresar Edad")
+sCarrera = input("Ingresar Carrera")
+SCarne = input("Ingresar carnet")
+#Objetivo
+print("nombre", sNombre)
+print("edad", sEdad)
+print("carrera", sCarrera)
+print("carne", SCarne)
+#salida
+print("Soy", sNombre, "Tengo", sEdad, "años, estudio", sCarrera, "Mi numero de carne es", SCarne, ",este es mi segundo programa.")
